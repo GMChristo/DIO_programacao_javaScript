ed0acd7aad89abca69f3a23dfe0e16c38e8fdda3
Gabriel Christo <gabrielchristo@hotmail.com>
2021-02-25 10:53:17 -0300

    Desenvolva páginas web com JavaScript

--- js/main.js
+++ js/main.js
@@ -50,3 +50,56 @@
 // console.log(d);
 // console.log(d.getMonth()+1); //no caso do mes sempre adicionar 1
 // console.log(d.getMinutes());
+
+
+// -----Desenvolva páginas web com JavaScript-----
+
+// function soma(n1, n2){
+//     return n1 + n2;
+// }
+// function setReplace(frase, nome, novo_nome){
+//     return frase.replace(nome, novo_nome);
+// }
+// function validaIdade(idade){
+//     var validar;
+//     if(idade >= 18){
+//         validar = true;
+//     }else{
+//         validar = false;
+//     }
+//     return validar;
+// }
+// var idade = prompt("Digite sua idade: ");
+
+// console.log(soma(5, 10));
+// console.log(setReplace("Vai Japão", "Japão", "Brasil"));
+// console.log(validaIdade(idade));
+
+function clicou(){
+    document.getElementById("agradecimento").innerHTML = "<b>Obrigado por clicar!</b>";
+    // alert("Obrigado por clicar!");
+}
+
+function redirecionar(){
+    window.open("https://github.com/GMChristo"); //nova aba
+    // window.location.href = "https://github.com/GMChristo"; //msm aba
+}
+
+function trocar(element){
+    element.innerHTML = "Obrigado por passar o mouse!";
+    // document.getElementById("mouseMove").innerHTML = "Obrigado por passar o mouse!";
+    // alert("trocar texto");
+}
+
+function voltar(element){
+    element.innerHTML = "Passe o mouse aqui!";
+    document.getElementById("mouseMove").innerHTML = "Passe o mouse aqui!";
+}
+
+function load(){
+    alert("Pagina carregada!");
+}
+
+function funcaoChange(elemento){
+    console.log(elemento.value);
+}
