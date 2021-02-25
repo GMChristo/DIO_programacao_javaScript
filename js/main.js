c275ee556d561918b1cdca19afad11df549a4f7b
Gabriel Christo <gabrielchristo@hotmail.com>
2021-02-24 23:24:34 -0300

    Array e Dicionário

--- js/main.js
+++ js/main.js
@@ -1,7 +1,26 @@
-var nome = "Gabriel Christo";
-var idade = 31;
-var frase = "Japão é o melhor time do mundo!"
+// var nome = "Gabriel Christo";
+// var idade = 31;
+// var frase = "Japão é o melhor time do mundo!"
 // alert(nome + " tem " + idade + " anos");
-console.log(nome);
-console.log(idade);
-console.log(frase.replace("Japão", "Brasil"));
+// console.log(nome);
+// console.log(idade);
+// console.log(frase.replace("Japão", "Brasil"));
+
+//-----Array e Dicionario-----
+
+// var lista = ["maça", "pêra", "laranja"];
+// lista.push("uva");
+// lista.pop();
+// console.log(lista);
+// console.log(lista.length);
+// console.log(lista.reverse());
+// console.log(lista.toString());
+// console.log(lista.join(" - "));
+
+//dicionario
+// var fruta = {nome: "maçã", cor: "vermelha"}
+// console.log(fruta.nome);
+
+var frutas = [{nome: "maçã", cor: "vermelha"}, {nome: "uva", cor: "roxa"}]
+console.log(frutas);
+console.log(frutas[1].nome);
