f732f69f42b8f78b13f4816bb9272234a45d1892
Gabriel Christo <gabrielchristo@hotmail.com>
2021-02-24 23:14:07 -0300

    Introdução ao JavaScript

---
+++ js/main.js
@@ -0,0 +1,7 @@
+var nome = "Gabriel Christo";
+var idade = 31;
+var frase = "Japão é o melhor time do mundo!"
+// alert(nome + " tem " + idade + " anos");
+console.log(nome);
+console.log(idade);
+console.log(frase.replace("Japão", "Brasil"));
